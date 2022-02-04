63abe6d5e5f8b662272fbb19a91cff2ac571b830
ANDROMAQUE <nikita.gribkoff10.10@gmail.com>
2022-02-04 21:55:54 +0300

    add methods timeToWork and timeToBreak

--- pomodoro-app/bin/pomodoro_app.dart
+++ pomodoro-app/bin/pomodoro_app.dart
@@ -1,3 +1,50 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:async';
+
+main() {
+  timeToWork();
+}
+
+Timer interval(Duration duration, func) {
+  Timer function() {
+    Timer timer = Timer(duration, function);
+    func(timer);
+    return timer;
+  }
+
+  return Timer(duration, function);
+}
+
+//* 1 minutes = 60 seconds *//
+// void converter() {
+//   final forMin = 60;
+//   final forHourse = 360;
+// }
+
+void timeToWork() {
+  const timeForWork = 1;
+  const converterForMin = timeForWork * 10;
+  int i = 0;
+  print('You have $timeForWork minutes for Work! Good Luke!');
+  interval(Duration(seconds: 1), (timer) {
+    print(i++);
+    if (i > converterForMin) {
+      print('Time to take a break!');
+      timer.cancel();
+      timeToBreak();
+    }
+  });
+}
+
+void timeToBreak() {
+  const timeForBreak = 1;
+  const converterForMin = timeForBreak * 10;
+  int i = 0;
+  print('You have $timeForBreak minutes for break');
+  interval(Duration(seconds: 1), (timer) {
+    print(i++);
+    if (i > converterForMin) {
+      print("It's time to work!");
+      timer.cancel();
+    }
+  });
 }
